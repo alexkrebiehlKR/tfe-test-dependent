9a471a118cdeba82fa6d764fe73fc305ed47d053
Alex Krebiehl <alex.krebiehl@kroger.com>
2021-07-22 15:06:37 -0400

    test

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
 resource "null_resource" "foo" {}
+
+data "terraform_remote_state" "test" {
+    workspace = "alexs-tfe-test"
+}
+
+output "test" {
+    value = "hello ${data.terraform_remote_state.test.hello}"
+}
