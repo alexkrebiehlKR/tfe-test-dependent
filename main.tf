1af904b77a00fc52cc6cf2fb95e006adaf077246
Alex Krebiehl <alex.krebiehl@kroger.com>
2021-12-06 15:41:24 -0500

    foo

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
-resource "null_resource" "foo" {}
+variable "remote_ws_name" {}
 
-output "test" {
-  value = "hello test3"
+data "terraform_remote_state" "bootstrap" {
+  backend = "remote"
+
+  config = {
+    organization = "hashicorp"
+    workspaces = {
+      name = var.remote_ws_name
+    }
+  }
+}
+
+resource "local_file" "foo" {
+    content     = data.terraform_remote_state.bootstrap.github_repo_id
+    filename = "foo.bar"
 }
